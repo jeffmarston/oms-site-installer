9396ce0547958cb29e8c040bc6d137e7b563e71a
Jeff Marston <jmarston@gmail.com>
2019-05-26 21:39:42 -0400

    missed file

---
+++ client/src/views/diagnostics/Logs.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="animated fadeIn">
+    <b-row>
+      <b-col cols="12" md="12">
+        <b-form>
+          <b-form-group label="Command" label-for="commandText" :label-cols="2">
+            <b-form-select
+              id="commandText"
+              :plain="true"
+              :options="[
+                  'Parse Trading Logs',
+                  'Parse Analytics Logs',
+                  'Parse Compliance Logs']"
+            ></b-form-select>
+          </b-form-group>
+        </b-form>
+        <b-textarea style="height: 700px"></b-textarea>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "data integrity",
+  data() {
+    return {
+      dataChecks: [{ name: "", checked: false }]
+    };
+  },
+  methods: {
+    showAlert() {
+      this.dismissCountDown = this.dismissSecs;
+    }
+  }
+};
+</script>
